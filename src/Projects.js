89c19c0e187748706b1559aac64c14afe18b0113
Shankarprince <shankarsowmiya07@gmail.com>
2022-02-17 12:36:11 +0530

    committed

--- src/Projects.js
+++ src/Projects.js
@@ -9,9 +9,9 @@ export function Projects() {
                     <p><span>Libraries and Packages Used :</span> React, React DOM, React-Router-DOM, Express, JsonWebToken, MongoDB, Dotenv, Cors, Bcrypt, Nodemon</p>
                     <p><span>Database Used :</span> MongoDB Atlas</p>
                     <p><span>Environment Used :</span> Visual Studio Code, NodeJS</p>
-                    <p><span>Deployed Link : <a href="https://petty-cash-manager.netlify.app">https://petty-cash-manager.netlify.app</a></span></p>
-                    <p><span>Source Code Link : <a href="https://github.com/Shankarprince/petty-cash-manager-client">https://github.com/Shankarprince/petty-cash-manager-client</a></span></p>
-                    <p><span>Source Code Link : <a href="https://github.com/Shankarprince/petty-cash-manager-server">https://github.com/Shankarprince/petty-cash-manager-server</a></span></p>
+                    <p><span>Deployed Link : <a rel="noreferrer" target="_blank" href="https://petty-cash-manager.netlify.app">https://petty-cash-manager.netlify.app</a></span></p>
+                    <p><span>Source Code Link : <a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince/petty-cash-manager-client">https://github.com/Shankarprince/petty-cash-manager-client</a></span></p>
+                    <p><span>Source Code Link : <a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince/petty-cash-manager-server">https://github.com/Shankarprince/petty-cash-manager-server</a></span></p>
                 </div>
                 <div className="project-container">
                     <h2>Ecommerce App</h2>
@@ -19,9 +19,9 @@ export function Projects() {
                     <p><span>Libraries and Packages Used :</span> React, React DOM, React-Router-DOM, Express, JsonWebToken, MongoDB, Dotenv, Cors, Bcrypt, Nodemon</p>
                     <p><span>Database Used :</span> MongoDB Atlas</p>
                     <p><span>Environment Used :</span> Visual Studio Code, NodeJS</p>
-                    <p><span>Deployed Link : <a href="https://electrokart.netlify.app">https://electrokart.netlify.app</a></span></p>
-                    <p><span>Source Code Link : <a href="https://github.com/Shankarprince/ecommerce-client">https://github.com/Shankarprince/ecommerce-client</a></span></p>
-                    <p><span>Source Code Link : <a href="https://github.com/Shankarprince/ecommerce-server">https://github.com/Shankarprince/ecommerce-server</a></span></p>
+                    <p><span>Deployed Link : <a rel="noreferrer" target="_blank" href="https://electrokart.netlify.app">https://electrokart.netlify.app</a></span></p>
+                    <p><span>Source Code Link : <a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince/ecommerce-client">https://github.com/Shankarprince/ecommerce-client</a></span></p>
+                    <p><span>Source Code Link : <a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince/ecommerce-server">https://github.com/Shankarprince/ecommerce-server</a></span></p>
                 </div>
                 <div className="project-container">
                     <h2>Movies App</h2>
@@ -29,9 +29,9 @@ export function Projects() {
                     <p><span>Libraries and Packages Used :</span> React, React DOM, React-Router-DOM, Express, JsonWebToken, MongoDB, Dotenv, Cors, Bcrypt, Nodemon</p>
                     <p><span>Database Used :</span> MongoDB Atlas</p>
                     <p><span>Environment Used :</span> Visual Studio Code, NodeJS</p>
-                    <p><span>Deployed Link : <a href="https://movie-reviews-app.netlify.app">https://movie-reviews-app.netlify.app</a></span></p>
-                    <p><span>Source Code Link : <a href="https://github.com/Shankarprince/movie-reviews-client">https://github.com/Shankarprince/movie-reviews-client</a></span></p>
-                    <p><span>Source Code Link : <a href="https://github.com/Shankarprince/movie-reviews-server">https://github.com/Shankarprince/movie-reviews-server</a></span></p>
+                    <p><span>Deployed Link : <a rel="noreferrer" target="_blank" href="https://movie-reviews-app.netlify.app">https://movie-reviews-app.netlify.app</a></span></p>
+                    <p><span>Source Code Link : <a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince/movie-reviews-client">https://github.com/Shankarprince/movie-reviews-client</a></span></p>
+                    <p><span>Source Code Link : <a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince/movie-reviews-server">https://github.com/Shankarprince/movie-reviews-server</a></span></p>
                 </div>
                 <div className="project-container">
                     <h2>Video Converter Desktop App</h2>
@@ -39,7 +39,7 @@ export function Projects() {
                     <p><span>Libraries and Packages Used :</span> Electron, Ffmpeg, Nodemon</p>
                     <p><span>Database Used :</span> Not Applicable</p>
                     <p><span>Environment Used :</span> Visual Studio Code, NodeJS</p>
-                    <p><span>Source Code Link : <a href="https://github.com/Shankarprince/video-converter">https://github.com/Shankarprince/video-converter</a></span></p>
+                    <p><span>Source Code Link : <a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince/video-converter">https://github.com/Shankarprince/video-converter</a></span></p>
                 </div>
             </section>
         </div>
