0c45ff9f07d4befc1a493093d3bc73c9e8c7ed52
Shankar <89457569+Shankarprince@users.noreply.github.com>
2022-02-18 12:08:57 +0530

    resume updated

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ export function Contact() {
                 <p><span>Email : </span>shankarsowmiya07@gmail.com</p>
                 <p><span>Github : </span><a rel="noreferrer" target="_blank" href="https://github.com/Shankarprince">https://github.com/Shankarprince</a></p>
                 <p><span>LinkedIn : </span><a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/shankarprince/">https://www.linkedin.com/in/shankarprince/</a></p>
-                <p><span>Resume : </span><a rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1hzifbzsZlgmnJSQBMxK97q-sFiPZ8x9V/view?usp=sharing">Google Drive Link</a></p>
+                <p><span>Resume : </span><a rel="noreferrer" target="_blank" href="https://drive.google.com/drive/folders/1CMBMvrHjrZa9iNgtawOdR7zPOwlsR6dH?usp=sharing">Google Drive Link</a></p>
             </div>
         </div>
     );
